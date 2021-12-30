43f9309bc136ddb71cf8fca69695ac213c473abc
ImNotAntMan <ikarusppong@gmail.com>
2021-12-30 23:13:19 +0900

    Update memo_modifyprocess.php

--- toy_memo/memo_modifyprocess.php
+++ toy_memo/memo_modifyprocess.php
@@ -15,6 +15,29 @@
   // db연결 준비
   require "./dbconfig.php";
 
+  function stmtupdate($i, $subject, $contents, $memoid) {
+    if($i == 0) {
+        $msg = "제목, 내용 변경";
+        $stmt = $conn->prepare("UPDATE toymemo SET subject = ?, contents = ?  WHERE memoid = ?" );
+        $stmt->bind_param("sss", $subject, $contents, $memoid);
+    } else if ($i == 1) {
+        $msg = "내용 변경";
+        $stmt = $conn->prepare("UPDATE toymemo SET contents = ? WHERE memoid = ?" );
+        $stmt->bind_param("ss", $contents, $memoid);
+    } else if ($i == 2) {
+        $msg = "제목 변경";
+        $stmt = $conn->prepare("UPDATE toymemo SET subject = ?  WHERE memoid = ?" );
+        $stmt->bind_param("ss", $subject, $memoid);
+    }
+    $stmt->execute();
+  }
+
+  function stmtinsert($userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg) {
+        $stmt = $conn->prepare("INSERT INTO toymemoupdate(userid, memoid, modifydate, subject, contents, modify) VALUES(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
+        $stmt->execute();    
+  }
+
   // 데이터베이스 작업 전, 메모 수정 화면으로 부터 값을 전달 받고
   $memoid = $_POST['memoid'];
   $userid = $_POST['userid'];
@@ -26,41 +49,26 @@
   $sql = "select * from toymemo where memoid=".$memoid." order by registdate desc";
   $resultset = $conn->query($sql);
   if($resultset->num_rows > 0) {
-      $row = $resultset->fetch_assoc();
-      $originalsubject = $row['subject'];
-      $originalcontents = $row['contents'];
-  }
+    $row = $resultset->fetch_assoc();
+    $originalsubject = $row['subject'];
+    $originalcontents = $row['contents'];
+}
 
   // 두 값을 비교하여 수정 사항이 어떤건지 알아내자.
   if(($subject != $originalsubject) && ($contents != $originalcontents)) {
-    $msg = "제목, 내용 변경";
-    $stmt = $conn->prepare("UPDATE toymemo SET subject = ?, contents = ?  WHERE memoid = ?" );
-    $stmt->bind_param("sss", $subject, $contents, $memoid);
-    $stmt->execute();
+        $i = 0; // 제목, 내용 변경
+        stmtupdate($i, $subject, $contents, $memoid);
+        stmtinsert($userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
 
-    $stmt = $conn->prepare("INSERT INTO toymemoupdate(userid, memoid, modifydate, subject, contents, modify) VALUES(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
-    $stmt->execute();    
-  } else if (($subject == $originalsubject) && ($contents != $originalcontents)) {
-    $msg = "내용 변경";
-    $stmt = $conn->prepare("UPDATE toymemo SET contents = ?  WHERE memoid = ?" );
-    $stmt->bind_param("si", $contents, $memoid);
-    $stmt->execute();
-      
-    $stmt = $conn->prepare("INSERT INTO toymemoupdate(userid, memoid, modifydate, subject, contents, modify) VALUES(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
-    $stmt->execute();
-      
+    } else if (($subject == $originalsubject) && ($contents != $originalcontents)) {
+        $i = 1; // 내용 변경
+        stmtupdate($i, $subject, $contents, $memoid);
+        stmtinsert($userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
   } else if(($subject != $originalsubject) && ($contents == $originalcontents)) {
-    $msg = "제목 변경";
-    $stmt = $conn->prepare("UPDATE toymemo SET subject = ? WHERE memoid = ?" );
-    $stmt->bind_param("ss", $subject, $memoid);
-    $stmt->execute();
-    
-    $stmt = $conn->prepare("INSERT INTO toymemoupdate(userid, memoid, modifydate, subject, contents, modify) VALUES(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
-    $stmt->execute();
-  
+    $i = 2; // 제목 변경
+    stmtupdate($i, $subject, $contents, $memoid);
+    stmtinsert($userid, $memoid, $registdate, $originalsubject, $originalcontents, $msg);
+
   } else {
     $msg = "뭘 하실려구??";
     echo "<script>alert('".$msg."')</script>";
@@ -70,5 +78,5 @@
   $conn->close();
 
   // 프로세스 플로우를 인덱스 페이지로 돌려준다.
-  header('Location: memo_view.php?userid='.$userid.'&memoid='.$memoid);
+  header('Location: index.php?userid='.$userid.'&memoid='.$memoid);
 ?>
